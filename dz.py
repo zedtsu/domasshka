0ba3afe1174163e9d372795d77cca39bf5def482
Sasha <sashok.ey@mail.ru>
2023-05-24 15:39:29 +0800

    gotovovov

--- dz.py
+++ dz.py
@@ -1,30 +1,67 @@
 import random
+import telebot
 
 def zadacha1():
-    # Задача 1. Задайте список случайных чисел от 1 до 10, выведите все элементы больше 5. Используйте для решения лямбда-функцию.
-    spisok = [random.randint(1, 10) for i in range(int(input('Введите размер списка: ')))]
-    print(spisok)
-    otvet = lambda x: x > 5
-    spisok = list(filter(otvet, spisok))
-    print('Результат: ', spisok)
+    # Задача 1. Создайте пользовательский аналог метода map().
+    def sum(a):
+        return a + 5
 
+    spisok = [random.randint(0, 10) for i in range(10)]
+    print(spisok)
+    
+    for i in spisok:
+        a = map(sum, spisok)
+    print(list(a))
+
+    spisok2 = []
+
+    for i in spisok:
+        spisok2.append(i + 5)
+    print(spisok2)
 
 def zadacha2():
-    #Задача 2. Дан список случайных чисел. Создайте список, в который попадают числа, описывающие случайную возрастающую последовательность. Порядок элементов менять нельзя.
-    spisok = [random.randint(1, 10) for i in range(int(input('Введите размер списка: ')))]
-    print('Начальный результат: ', *spisok)
-    spisok2 = [(len(spisok))]
+    # Задача 2. Создайте декоратор, повторяющий функцию заданное количество раз.
+    def summa(func):
+        def decorator(a):
+            print(f'{a} + {a} = ', end=' ')
+            func(a)
+        return decorator
 
-    for i in range(len(spisok) // 3):
-        spisok2.append(spisok[random.randint(0, len(spisok) - 1)])
-    spisok2.sort()
-    spisok2 = set(spisok2)
-    print('Конечный результат: ', *spisok2)
+    @summa
+    def sum(a):
+        print(a + 1)
+
+    l = input('Введите сколько раз вы хотите вызвать функицю: ')
+    l = int(l)
+
+    for i in range(l):
+        sum(i)
+
 
 def zadacha3():
-    # Задача 3. Задайте список случайных чисел от 1 до 10. Посчитайте, сколько всего совпадающих элементов есть в списке. Удалите все повторяющиеся элементы.
-    dlina = n = int(input(''))
-    spisok = [random.randint(1, 10) for i in range(n)]
-    print(spisok)
-    spisok = set(spisok)
-    print(f'Результат: {spisok}. Удалено повторяющихся элементов: {dlina - len(spisok)}')
+    # Задача 3. Добавьте в telegram-бота игру «Угадай числа». Бот загадывает число от 1 до 1000. Когда игрок угадывает его, бот выводит количество сделанных ходов.
+    bot = telebot.TeleBot("6222578607:AAHO37nA3Ezse6VEfNe60grtgTNR0Mgrnik")
+
+    @bot.message_handler(commands=['game'])
+    def start(message):
+        bot.send_message(message.chat.id, 'Отгдадайте число от 1 до 1000.')
+
+        global number
+        number = random.randint(1, 1000)
+
+    @bot.message_handler(content_types=['game'])
+    def game(message):
+        global number
+        attemps = 0
+        if message.text.isdigit():
+            guess = int(message.text)
+            if guess == number:
+                bot.send_message(message.chat.id, f'Поздравляю! Вы угадали число за {attempts} попыток.')
+            elif guess > number:
+                bot.send_message(message.chat.id, 'Загаданное число меньше.')
+            elif guess < number:
+                bot.send_message(message.chat.id, 'Загаданное число больше.')
+            attempts = attemps + 1
+        else:
+            bot.send_message(message.chat.id, 'Пожалуйста, введите число от 1 до 1000.')
+    bot.polling()
